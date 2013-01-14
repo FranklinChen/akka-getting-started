0771a4737321f606ec928a167d3a29b9e298f8b2
Franklin Chen <franklinchen@franklinchen.com>
2013-01-14 15:28:54 -0500

    Initial.

---
+++ README.md
@@ -0,0 +1,12 @@
+# A sample for the [Typesafe Stack](http://typesafe.com/stack)
+
+Akka sample project using Scala and SBT.
+
+Used [Typesafe Akka getting started tutorial](http://typesafe.com/resources/tutorials/getting-started-with-akka-scala.html) but updated to Akka 2.1.0.
+
+To run and test it use SBT invoke:
+
+```
+$ sbt run
+```
+
---
+++ src/main/scala/com/franklinchen/AkkaGettingStarted.scala
@@ -0,0 +1,37 @@
+package com.franklinchen
+
+import akka.actor._
+import akka.pattern.ask
+import scala.concurrent.duration._
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
+
+case object Tick
+case object Get
+
+class Counter extends Actor {
+  var count = 0
+
+  def receive = {
+    case Tick => count += 1
+    case Get  => sender ! count
+  }
+}
+
+object AkkaGettingStarted extends App {
+  val system = ActorSystem("AkkaGettingStarted")
+
+  val counter = system.actorOf(Props[Counter])
+
+  counter ! Tick
+  counter ! Tick
+  counter ! Tick
+
+  implicit val timeout = Timeout(5 seconds)
+
+  (counter ? Get) onSuccess {
+    case count => println("Count is " + count)
+  }
+
+  system.shutdown()
+}
